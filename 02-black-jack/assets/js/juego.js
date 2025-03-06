930e5048278e74e2ceba45c3a3bcc13db7e0ee91
Jason <luxus@Jasons-MacBook-Pro.local>
2025-03-06 23:21:00 +0100

    crear cartas HTML done

--- 02-black-jack/assets/js/juego.js
+++ 02-black-jack/assets/js/juego.js
@@ -4,6 +4,18 @@ let deck = [];
 let tipos = ['C', 'D', 'H', 'S'];
 let especials = ['A', 'J', 'Q', 'K'];
 
+
+const btnNew  = document.querySelector('#btnNew');
+const btnDraw = document.querySelector('#btnDraw');
+const btnStop = document.querySelector('#btnStop');
+
+const htmlPts = document.querySelectorAll('small');
+const playerCards = document.querySelector('#player-cards');
+const machineCards = document.querySelector('#machine-cards');
+
+let playerPts  = 0;
+let machinePts = 0;
+
 const makeDeck = () => {
 	for (let i = 2; i <= 10; i++){
 		for(let tipo of tipos){
@@ -23,6 +35,48 @@ makeDeck();
 
 const drawCard = () => {
 	deck = _.shuffle(deck);
-	let newCard = deck.shift();
+	let newCard = deck.pop();
 	return newCard;
 }
+
+const cardValue = (carta) => {
+	const value = carta.substring(0, carta.length - 1);
+	return (isNaN(value)) ?
+			(isNaN(value) === 'A') ? 11 : 10
+			: value * 1;
+	/* let puntos = 0;
+	if (isNaN(value)){
+		console.log("No es un número");
+		puntos = (value === 'A') ? 11 : 10;
+	} else {
+		console.log('Es un número');
+		puntos = value * 1;
+	}
+	console.log(puntos); */
+}
+
+/* const valor = cardValue(drawCard()); */
+/* console.log({valor}); */
+
+btnDraw.addEventListener('click', () => {
+	const card = drawCard();
+	playerPts  = playerPts + cardValue(card);
+	console.log(playerPts);
+	htmlPts[0].innerText = playerPts;
+
+	const newCard = document.createElement('img')
+	newCard.src = `./assets/cartas/${card}.png`;
+	newCard.classList.add('carta');
+	playerCards.append(newCard);
+
+	if (playerPts > 21){
+		btnDraw.disabled = true;
+		console.warn("Perdiste!")
+	} else if (playerPts === 21){
+		btnDraw.disabled = true;
+		console.warn("21, genial!!")
+	}
+
+
+});
+
